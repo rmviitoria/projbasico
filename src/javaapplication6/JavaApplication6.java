c3e66f7b5d0de7f42bc5686ca584b1b7c8f679b6
Vitória Reis <vitoria.mr2000@gmail.com>
2021-05-07 19:42:03 -0300

    Projeto criado dia 04/05/2021 - questionário de informática.

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor1= 1;
+                int valor2 = 2;
+                
+                Scanner scan = new Scanner(System.in);
+                
+                System.out.println("");
+    }
+    
+}
